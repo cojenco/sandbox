72608b5d213b5fedc5c23a529f9b24fd5f1328c0
Cathy Ouyang <cathyo@google.com>
2021-08-17 11:32:10 -0700

    cleanup: simplify sandbox to use anonymous credentials

--- test_retries.py
+++ test_retries.py
@@ -1,19 +1,24 @@
 import requests
-import io
 import json
 import http
 import uuid
-http.client.HTTPConnection.debuglevel=5
 
 from google.cloud import storage
+from google.auth.credentials import AnonymousCredentials
+
+# Uncomment line below to enable logging.DEBUG
+# http.client.HTTPConnection.debuglevel=5
 
 
+# Emulator host
 _API_ACCESS_ENDPOINT = "http://127.0.0.1:9000"
+
+# Fake project and service accounts for emulator use
 _CONF_TEST_PROJECT_ID = "my-project-id"
 _CONF_TEST_SERVICE_ACCOUNT_EMAIL = "my-service-account@my-project-id.iam.gserviceaccount.com"
 
 
-# === Interaction with Retry Test API === #
+# === Interact with Retry Test API === #
 
 
 def _create_retry_test(method_name, instructions):
@@ -28,34 +33,32 @@ def _create_retry_test(method_name, instructions):
     }
     data = json.dumps(data_dict)
     r = requests.post(preflight_post_uri, headers=headers, data=data)
-    print(r.text)
+    (r.text)
     return r.json()
 
 
 def _get_retry_test(id):
     status_get_uri = "{base}{retry}/{id}".format(base=_API_ACCESS_ENDPOINT, retry="/retry_test", id=id)
     r = requests.get(status_get_uri)
-    print(r.text)
+    (r.text)
     return r.json()
 
 
 def _run_retry_test(id, func, _preconditions, **resources):
-    client = storage.Client(client_options={"api_endpoint": _API_ACCESS_ENDPOINT})
+    # create a new client to send the retry test ID using the x-retry-test-id header in each request
+    client = storage.Client(
+        project=_CONF_TEST_PROJECT_ID,
+        credentials=AnonymousCredentials(),
+        client_options={"api_endpoint": _API_ACCESS_ENDPOINT},
+    )
     client._http.headers.update({"x-retry-test-id": id})
     func(client, _preconditions, **resources)
-    # try:
-    #     func(client, _preconditions, **resources)
-    #     print("we made it!")
-    # except Exception as e:
-    #     print("uhoh")
-    #     print(type(e))
-    #     print(e)
+
 
 
 def _delete_retry_test(id):
     status_get_uri = "{base}{retry}/{id}".format(base=_API_ACCESS_ENDPOINT, retry="/retry_test", id=id)
     r = requests.delete(status_get_uri)
-    print(r.text)
 
 
 # === Populate Resources/Fixtures === #
@@ -68,9 +71,8 @@ def _populate_resource_bucket(client, resources):
 
 def _populate_resource_object(client, resources):
     bucket_name = resources["bucket"].name
-    bucket = client.get_bucket(bucket_name)
-    blob = bucket.blob(uuid.uuid4().hex)
-    blob.upload_from_string("hello world")
+    blob = client.bucket(bucket_name).blob(uuid.uuid4().hex)
+    blob.upload_from_string("hello world", checksum="crc32c")
     blob.reload()
     resources["object"] = blob
 
@@ -104,188 +106,85 @@ def _populate_resource(client, json_resource):
             func = resource_mapping[r]
             func(client, resources)
         except Exception as e:
-            print("log warning here: {}".format(e))
+            ("log warning here: {}".format(e))
 
     return resources
 
 
-# === Library Methods === #
+# === Library Methods for Sandbox Use=== #
 
 
-def test_iam_permissions(client, _preconditions, bucket):
-    bucket = client.bucket(bucket.name)
-    permissions = ["storage.buckets.get", "storage.buckets.create"]
-    bucket.test_iam_permissions(permissions)
+def client_get_bucket(client, _preconditions, **resources):
+    bucket = resources.get("bucket")
+    client.get_bucket(bucket.name)
 
-def list_blobs(client, _preconditions, bucket, **_):
-    blobs = client.list_blobs(bucket.name)
-    for b in blobs:
-        break
+def bucket_create(client, _preconditions, **_):
+    bucket = client.bucket(uuid.uuid4().hex)
+    bucket.create()
 
-def get_blob(client, _preconditions, bucket, **_):
-    bucket = client.bucket(bucket.name)
-    bucket.get_blob(object.name)
-
-def get_bucket(client, _preconditions, bucket):
-    bucket = client.get_bucket(bucket.name)
-
-
-def make_bucket_public(client, _preconditions, bucket):
-    bucket = client.bucket(bucket.name)
-    bucket.make_public()
-
-def make_blob_public(client, _preconditions, bucket, object):
-    blob = client.bucket(bucket.name).blob(object.name)
-    blob.make_public()
-
-def get_bucket_acl(client, _preconditions, bucket):
-    bucket = client.bucket(bucket.name)
-    bucket_acl = bucket._acl
-    entity = bucket_acl.entity("allUsers")
-    bucket_acl.reload()
-    print(bucket_acl.has_entity(entity))
-
-def get_service_account_email(client, _preconditions, **_):
-    client.get_service_account_email()
-
-
-def patch_bucket(client, _preconditions, bucket):
-    bucket = client.bucket(bucket.name)
-    metadata = {"foo": "bar"}
-    bucket.metadata = metadata
-    if _preconditions:
-        bucket.reload()
-        metageneration = bucket.metageneration
-        print("!!!!!")
-        print(metageneration)
-        bucket.patch(if_metageneration_match=metageneration)
-    else:
-        bucket.patch()
-
-
-def list_buckets(client, _preconditions, **_):
+def client_list_buckets(client, _preconditions, **_):
     buckets = client.list_buckets()
     for b in buckets:
-        print(b)
+        pass
 
-# storage.objects.copy
-def rename_blob(client, _preconditions, bucket, object):
-    bucket = client.bucket(bucket.name)
-    new_name = "new_name"
-    if _preconditions:
-        generation = object.generation
-        print(generation)
-        bucket.rename_blob(object, new_name, if_generation_match=generation)
-    else:
-        bucket.rename_blob(object, new_name)
-
-# storage.objects.copy
-def copy_blob(client, _preconditions, bucket, object):
-    bucket = client.bucket(bucket.name)
-    dest = client.bucket("bucket")
-    new_name = "new_name"
-    if _preconditions:
-        generation = object.generation
-        print(generation)
-        bucket.copy_blob(object, dest, new_name=new_name, if_generation_match=object.generation)
-    else:
-        bucket.copy_blob(object, dest, new_name=new_name)
-
-# storage.objects.delete
-def delete_blob(client, _preconditions, bucket, object):
-    bucket = client.bucket(bucket.name)
-    if _preconditions:
-        generation = object.generation
-        print(generation)
-        bucket.delete_blob(object.name, if_generation_match=generation)
-    else:
-        bucket.delete_blob(object.name)
-
-# storage.buckets.lockRententionPolicy
-def lock_retention_policy(client, _preconditions, bucket):
-    bucket2 = client.bucket("bucket")
-    bucket2.retention_period = 60
-    bucket2.patch()
-    bucket2.lock_retention_policy()
-
-# storage.objects.insert
-def upload_from_string(client, _preconditions, bucket):
-    blob = client.bucket(bucket.name).blob(uuid.uuid4().hex)
-    if _preconditions:    
-        blob.upload_from_string("upload from string", if_metageneration_match=0)
-    else:
-        blob.upload_from_string("upload from string")
-
-# Q: object generation is randomly created in the emulator
-# storage.objects.copy
-def copy_blob(client, _preconditions, bucket, object):
-    bucket = client.bucket(bucket.name)
+def bucket_copy_blob(client, _preconditions, **resources):
+    object = resources.get("object")
+    bucket = client.bucket(resources.get("bucket").name)
     destination = client.bucket("bucket")
-    new_name = "new_name"
     if _preconditions:
-        bucket.copy_blob(object, destination, new_name=new_name, if_generation_match=0)
+        bucket.copy_blob(
+            object, destination, new_name=uuid.uuid4().hex, if_generation_match=0
+        )
     else:
         bucket.copy_blob(object, destination)
 
-# Q: google.protobuf.json_format.ParseError
-# storage.objects.compose
-def compose_blob(client, _preconditions, bucket, object):
-    blob_2 = bucket.blob("blob2")
-    blob_2.upload_from_string("second blob coming")
-    sources = [blob_2]
-    generations = [blob_2.generation]
-    
+def bucket_get_notification(client, _preconditions, **resources):
+    bucket = resources.get("bucket")
+    notification = resources.get("notification")
+    client.bucket(bucket.name).get_notification(notification.notification_id)
+
+def blob_delete(client, _preconditions, **resources):
+    bucket = resources.get("bucket")
+    object = resources.get("object")
+    blob = client.bucket(bucket.name).blob(object.name)
     if _preconditions:
-        object.compose(sources, if_generation_match=generations)
+        blob.delete(if_generation_match=object.generation)
     else:
-        object.compose(sources)
-
-# storage.buckets.setIamPolicy
-def set_iam_policy(client, _preconditions, bucket):
-    bucket = client.get_bucket("bucket")
-    role = "roles/storage.objectViewer"
-    member = _CONF_TEST_SERVICE_ACCOUNT_EMAIL
-
-    policy = bucket.get_iam_policy(requested_policy_version=3)
-    policy.bindings.append({
-        "role": role, 
-        "members": {
-            member
-        }
-    })
-
-    if _preconditions:
-        bucket.set_iam_policy(policy)
-    else:
-        bucket.set_iam_policy(policy)
+        blob.delete()
 
 
-# === Run Main Function === #
+# === Run Sandbox Main Test Function === #
 
 
-def test_emul_api():
+def test_emulator_retry_test_api():
 
-    ###### 1 - replace the correct endpoint(str)
-    method_name = "storage.objects.copy"
+    ###### 1 - replace the correct endpoint(str) and instructions
+    method_name = "storage.buckets.get"
     instructions = ["return-503", "return-503"]
 
     ###### 2 - fill in the correct preconditions(bool)
     _preconditions = True
 
-    ###### 3 - enter the correct resources needed
-    json_resource = ["BUCKET", "OBJECT"]
+    ###### 3 - default to populate all resources or enter only the needed resources
+    json_resource = ["BUCKET", "OBJECT", "NOTIFICATION", "HMAC_KEY"]
 
-    r = _create_retry_test(method_name, instructions)   # create retry test and get unique identifier
+    r = _create_retry_test(method_name, instructions)                      # create retry test and get unique identifier
     id = r["id"]
 
-    client = storage.Client(client_options={"api_endpoint": _API_ACCESS_ENDPOINT})
-    resources = _populate_resource(client, json_resource)   # populate resources
-    print(resources)
+    client = storage.Client(                                               # create a client with anonymous credentials to populate resources
+        project=_CONF_TEST_PROJECT_ID,
+        credentials=AnonymousCredentials(),
+        client_options={"api_endpoint": _API_ACCESS_ENDPOINT},
+    )
+    resources = _populate_resource(client, json_resource)                  # populate resources
 
     ###### 4 - replace get_bucket with the correct corresponding libarary method 
-    _run_retry_test(id, copy_blob, _preconditions, **resources)    # run library test
+    _run_retry_test(id, client_get_bucket, _preconditions, **resources)    # run library test
 
-    # get status with unique identifier
-    status_response = _get_retry_test(id)
+    status_response = _get_retry_test(id)                                  # get status with unique identifier
+    print(status_response)
 
-test_emul_api()
+    _delete_retry_test(id)                                                     # delete retry test
+
+
+test_emulator_retry_test_api()
